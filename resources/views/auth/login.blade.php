e3a62896f6672add0f0d1f8b2f5634e788743092
Muhammad Shariq Bin Zaid <shariqbinzaid@gmail.com>
2023-10-26 03:14:45 +0500

    logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,9 +64,9 @@
         <div class="d-flex flex-column flex-lg-row flex-column-fluid">
             <div class="d-flex flex-lg-row-fluid">
                 <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
-                    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
-                        src="{{ asset('assets/media/auth/agency.png') }}" alt="" />
-                    <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
+                    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-500px mb-10 mb-lg-20"
+                        src="{{ asset('assets/media/auth/logo.png') }}" alt="" />
+                    <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-500px mb-10 mb-lg-20"
                         src="{{ asset('assets/media/auth/agency-dark.png') }}" alt="" />
 
                     <h1 class="text-danger fs-2qx fw-bold text-center mb-7">Robert - Security App</h1>
@@ -81,7 +81,8 @@
                 </div>
             </div>
             <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
-                <div style="background-color:#f1416c47 !important;" class="bg-body d-flex flex-center rounded-4 w-md-600px p-10">
+                <div style="background-color:#f1416c47 !important;"
+                    class="bg-body d-flex flex-center rounded-4 w-md-600px p-10">
                     <div class="w-md-400px">
 
                         <form method="POST" action="{{ route('login') }}">
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -62,7 +62,7 @@
             <div class="d-flex flex-lg-row-fluid">
                 <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
                     <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
-                        src="{{ asset('assets/media/auth/agency.png') }}" alt="" />
+                        src="{{ asset('assets/media/auth/logo.png') }}" alt="" />
                     <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
                         src="{{ asset('assets/media/auth/agency-dark.png') }}" alt="" />
 
